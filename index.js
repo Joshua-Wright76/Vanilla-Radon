17062f4a7f99ce8476b59ff7fa6cd3da2f53c28a
ntsmitty <nicktsmith7@gmail.com>
2018-09-18 17:12:51 -0700

    megamarkets-demo

--- index.js
+++ index.js
@@ -3,11 +3,17 @@
 
 //template for market card
 const template = document.querySelector("#marketcard-temp")
+
 const locationTemplate = document.importNode(template.content, true);
+
+
 console.log(locationTemplate)
 
 //Button to add new location, creates new card and updates. On click the event is dispatched to a reducer. 
-const createNewLocation = () => document.getElementById("market-list").appendChild(locationTemplate);
+const createNewLocation = () => {
+  const locationTemplate = document.importNode(template.content, true);  
+document.getElementById("markets-container").appendChild(locationTemplate);
+}
 
 const addMarketCard = () => console.log('Here from addMarket')
 
